2a63cf9ea5f1843dba26bdda7388458bc4037e6e
ayushraj12009 <ayushraj12009@gmail.com>
2024-05-11 14:56:01 +0530

    Project setup done and working

---
+++ src/main/java/com/LensCorp/LensCorp/LensCorpApplication.java
@@ -0,0 +1,13 @@
+package com.LensCorp.LensCorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LensCorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LensCorp/LensCorp/LensCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LensCorp.LensCorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LensCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
